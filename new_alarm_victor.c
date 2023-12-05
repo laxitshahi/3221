7e4c67a865cc1afc0369f865bb0628363a9f60df
laxitshahi <laxitshahi@laxits-mbp.home>
2023-12-05 03:37:03 -0500

    added comments + few bugs discovered

--- new_alarm_victor.c
+++ new_alarm_victor.c
@@ -64,18 +64,23 @@ sem_t change_list_sem; // Semaphore for change alarm list
 void alarm_insert(alarm_t *alarm)
 {
     int status;
-    alarm_t **last, *next;
+    alarm_t **last, *next; 
 
     sem_wait(&alarm_list_sem); // Wait on the semaphore before accessing alarm_list
 
     /*
-     * LOCKING PROTOCOL:
+     * LOCKING PROTOCOL
      */
+
+    // Pointers to iterate through linked list
     last = &alarm_list;
     next = *last;
+
+    // Iterate through alarm_list (linked list)
     while (next != NULL)
     {
-        if (next->time >= alarm->time)
+        // If we find an alarm that fits in the correct slot, place alarm in linked list and break iteration
+        if (next->alarm_id >= alarm->alarm_id)
         {
             alarm->link = next;
             *last = alarm;
@@ -97,10 +102,9 @@ void alarm_insert(alarm_t *alarm)
 
     sem_post(&alarm_list_sem); // Post to the semaphore after modifying alarm_list
 
+    // On successful insertion, print message to user
     printf("Alarm(%d) Inserted by Main Thread %p Into Alarm List at %ld: Group(%d) %s\n",
            alarm->alarm_id, pthread_self(), (long)time(NULL), alarm->group_id, alarm->message);
-
-    pthread_cond_signal(&alarm_cond);
 
     /*
      * Wake the alarm thread if it is not busy (that is, if
@@ -108,6 +112,9 @@ void alarm_insert(alarm_t *alarm)
      * work), or if the new alarm comes before the one on
      * which the alarm thread is waiting.
      */
+
+    pthread_cond_signal(&alarm_cond);
+
     if (current_alarm == 0 || alarm->time < current_alarm)
     {
         current_alarm = alarm->time;
@@ -119,12 +126,12 @@ void alarm_insert(alarm_t *alarm)
 
 void change_alarm_insert(change_alarm_t *change_alarm)
 {
+    // Logic is the same as standard alarm_list insert
     change_alarm_t **last, *next;
 
     sem_wait(&change_list_sem); // Wait on the semaphore before accessing change_alarm_list
 
     // LOCKING PROTOCOL:
-
     last = &change_alarm_list;
     next = *last;
     while (next != NULL)
@@ -155,16 +162,22 @@ void change_alarm_insert(change_alarm_t *change_alarm)
 void assign_alarm_to_display_thread(alarm_t *alarm)
 {
     int assigned = 0;
+    // First we check if there is already a thread that is associated with the group_id of the given alarm
     for (int i = 0; i < MAX_DISPLAY_THREADS; i++)
     {
-        // Ensure that alarm is associated with the CORRECT group ID
+        // Ensure that the thread is active and the group_id of the alarm is equal to the groiup_id of the active display_thread
         if (display_threads[i].active == 1 && display_threads[i].group_id == alarm->group_id)
         {
             // Each display thread (which is associated with a particular group_id) should have MAX TWO alarms associated to it
+            // If there is < 2 alarms associated with this particular display threads...
             if (display_threads[i].alarm_count < 2)
             {
+
+            // 1. Increment the number of alarms this particular display thread handles
                 display_threads[i].alarm_count++;
+            // 2. Set assign flag to 1 so that a new thread is not create below
                 assigned = 1;
+            // 3. Print coresponding message to user
                 printf("Main Thread %p Assigned to Display Alarm(%d) at %ld: Group(%d) %s\n",
                        pthread_self(), alarm->alarm_id, (long)time(NULL), alarm->group_id, alarm->message);
                 break;
@@ -172,15 +185,18 @@ void assign_alarm_to_display_thread(alarm_t *alarm)
         }
     }
 
-    // If
-    // 1. If there does not exist any display thread associated with the alarm's group_id
+    // BUT If
+    // 1. If there does not exist any display thread associated with the alarm's group_id or..
     // 2. If the all display threads associated with the given alarm's group_id already have more than 1 alarm associated with it
     // Then create a new display_thread
     // @note, it is possible that multiple there are the same group_ids across multiple threads
     if (!assigned)
     {
+        // New display thread created with the associated group_id
         create_display_thread(alarm->group_id);
+        // Update the number of assigned threads to 1
         alarm->assigned_to_thread = 1;
+        // Print corresponding message to user
         printf("Main Thread Created New Display Alarm Thread %p For Alarm(%d) at %ld: Group(%d) %s\n",
                pthread_self(), alarm->alarm_id, (long)time(NULL), alarm->group_id, alarm->message);
     }
@@ -282,16 +298,23 @@ void *alarm_thread(void *arg)
 
 void create_display_thread(int group_id)
 {
+    // Loop through each display thread
     for (int i = 0; i < MAX_DISPLAY_THREADS; i++)
     {
+      // ONLY if this display thread is inactive (active == 0)
         if (display_threads[i].active == 0)
         {
+          // Setup display thread with given group_id
             display_threads[i].group_id = group_id;
             display_threads[i].active = 1;
             display_threads[i].alarm_count = 1;
 
+          
+            // Allocate space for the display thread 
             int *arg = malloc(sizeof(int));
             *arg = group_id;
+
+            // create new thread and pass in group_id into display_thread (thread function) WITH the group_id
             pthread_create(&display_threads[i].thread_id, NULL, display_thread, arg);
             // Add error handling for pthread_create
             break;
@@ -308,22 +331,36 @@ void *display_thread(void *arg)
     {
         sem_wait(&alarm_list_sem); // Wait on the semaphore before accessing alarm_list
 
+        
+
+        // Logic for when an alarm is removed
         removed_alarm_t **last = &removed_alarm_list, *current;
+
+        // Iterate through all removed alarms
         while ((current = *last) != NULL)
         {
+            // If the given group_id (from args) is equal to any of the removed alarms...
             if (current->group_id == group_id)
             {
+              // 1. Print message to users to notify them that the thread is no longer with the particular alarm
                 printf("Display Thread %p Has Stopped Printing Message of Alarm(%d) at %ld: Group(%d) %s\n",
                        pthread_self(), current->alarm_id, (long)current->removal_time, current->group_id, current->message);
+                
                 *last = current->link;
+              // Free memory used by alarm in removed_alarm_list
                 free(current);
             }
+          
+            // If no alarm is found that matches the group_id, simply iterate to the next value to check again
             else
             {
                 last = &current->link;
             }
         }
 
+
+
+        
         int found = 0;
         time_t now = time(NULL);
 
@@ -332,18 +369,28 @@ void *display_thread(void *arg)
         {
             if (alarm->group_id == group_id)
             {
+                /*
+                 * if alarm's group_id has been changed:
+                 * print message to user to notify and reset flag to unchanged (0)
+                */
                 if (alarm->original_group_id != 0)
                 {
                     printf("Display Thread %p Has Stopped Printing Message of Alarm(%d) at %ld: Changed Group(%d) %s\n",
                            pthread_self(), alarm->alarm_id, (long)now, alarm->original_group_id, alarm->message);
                     alarm->original_group_id = 0; // Reset after printing
                 }
-                else if (alarm->message_changed)
-                {
+                /*
+                 * if alarm's group_id has been changed:
+                 * print message to user to notify and reset flag to unchanged (0)
+                */
+                  else if (alarm->message_changed)
+                  {
                     printf("Display Thread %p Starts to Print Changed Message Alarm(%d) at %ld: Group(%d) %s\n",
                            pthread_self(), alarm->alarm_id, (long)now, alarm->group_id, alarm->message);
                     alarm->message_changed = 0; // Reset after printing
                 }
+                
+                // If there have be no changes to the alarm's group_id or message, print normal message that prints every 5 seconds
                 else
                 {
                     // Normal printing of the message
@@ -351,13 +398,14 @@ void *display_thread(void *arg)
                     printf("Alarm (%d) Printed by Alarm Display Thread %p at %ld: Group(%d) %d %s\n",
                            alarm->alarm_id, pthread_self(), (long)now, alarm->group_id, time_left, alarm->message);
                 }
+                // Update found flag to ensure that thread does not exit
                 found = 1;
             }
         }
 
         sem_post(&alarm_list_sem); // Post to the semaphore after reading alarm_list
 
-        // If no alarms were found for the group, exit the thread
+        // But If no alarms were found (0) for the group, exit the thread
         if (!found)
         {
             printf("No More Alarms in Group(%d): Display Thread %p exiting at %ld\n",
@@ -365,7 +413,7 @@ void *display_thread(void *arg)
             break;
         }
 
-        // Sleep for 5 seconds as per the requirements
+        // Sleep for 5 seconds as per the requirements (show message only every 5 seconds)
         sleep(5);
     }
 
@@ -382,10 +430,16 @@ int main(int argc, char *argv[])
     sem_init(&alarm_list_sem, 0, 1);  // Initialize semaphore for alarm list
     sem_init(&change_list_sem, 0, 1); // Initialize semaphore for change alarm list
 
+
+    // Start always running pthread that handles alarms
     status = pthread_create(
         &thread, NULL, alarm_thread, NULL);
+
+    // If creation of alarm fails, throw err and abort
     if (status != 0)
         err_abort(status, "Create alarm thread");
+
+    // Constantly running (Always looking for user input)
     while (1)
     {
         printf("Alarm> ");
@@ -394,25 +448,30 @@ int main(int argc, char *argv[])
 
         line[strcspn(line, "\n")] = 0; // Remove newline character
 
+        // If the user inputs 1 or less character, restart loop
         if (strlen(line) <= 1)
             continue;
 
+      
+        // If "Start_Alarm" command is called
         if (strncmp(line, "Start_Alarm", 11) == 0)
         {
             // Allocate memory for alarm
             alarm_t *alarm = (alarm_t *)malloc(sizeof(alarm_t));
+          // if allocation fails, throw err and abort
             if (alarm == NULL)
                 errno_abort("Allocate alarm");
 
-            // Check if all inputs are correct for Start_Alarm
-            // If portion of input format is incorrect, free memory and print error
+            // ensure all parts of the Start_Alarm command are correct
+            // If any parts of the Start_Alarm command is inconsistent with expected result, print error and free space allocated
             if (sscanf(line, "Start_Alarm(%d): Group(%d) %d %128[^\n]",
                        &alarm->alarm_id, &alarm->group_id, &alarm->seconds, alarm->message) < 4)
             {
                 fprintf(stderr, "Bad Start_Alarm command\n");
                 free(alarm);
             }
-            // If all inputs are correct..
+
+            // If all inputs are correct, setup alarm, assign to display thread, and insert into alarm_list
             else
             {
                 alarm->time = time(NULL) + alarm->seconds;
@@ -420,30 +479,42 @@ int main(int argc, char *argv[])
                 alarm_insert(alarm);
             }
         }
+
+        // If command for "Change_Alarm" called
+        // If portion of input format is incorrect, free memory and print error
         else if (strncmp(line, "Change_Alarm", 12) == 0)
         {
+            // Setup memory for change_alarm alarm
             change_alarm_t *change_alarm = (change_alarm_t *)malloc(sizeof(change_alarm_t));
+
+            // if allocation fails, throw err and abort
             if (change_alarm == NULL)
                 errno_abort("Allocate change alarm");
 
             int seconds;
+            // ensure all parts of the Start_Alarm command are correct
+            // If any parts of the Change_Alarm command is inconsistent with expected result, print error and free space allocated
             if (sscanf(line, "Change_Alarm(%d): Group(%d) %d %128[^\n]",
                        &change_alarm->alarm_id, &change_alarm->group_id, &seconds, change_alarm->message) < 4)
             {
                 fprintf(stderr, "Bad Change_Alarm command\n");
                 free(change_alarm);
             }
+
+            // If all inputs are correct, setup alarm and insert into change_alarm_list
             else
             {
                 change_alarm->time = time(NULL) + seconds;
                 change_alarm_insert(change_alarm);
             }
         }
+
+        // Reject all other inputs
         else
         {
             fprintf(stderr, "Invalid command\n");
         }
     }
-    // Cleanup and exit code
+
     return 0;
 }
